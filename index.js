76df8c94e17f120520c552b2780f59b165b22203
Mariafrancia Hernandez <mariafrancia.hg@gmail.com>
2026-02-09 22:19:25 -0300

    cambios

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ app.use(express.json());
 
 
 app.get("/", (req, res) => {
-  res.send("Servidor funcionando 🚀");
+  res.send(`Servidor funcionando 🚀 en puerto ${process.env.PORT || 3000}`);
 });
 
 
